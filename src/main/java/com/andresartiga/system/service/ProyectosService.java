7abbde2587e0cec9177c4c3274741415a6a7bf23
AndresArtiga333 <andresaartiga057@gmail.com>
2024-08-07 11:37:31 -0600

    feat: se agrego la funcion de listar proyectos en el service de proyectos

--- src/main/java/com/andresartiga/system/service/ProyectosService.java
+++ src/main/java/com/andresartiga/system/service/ProyectosService.java
@@ -1,6 +1,40 @@
 package com.andresartiga.system.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.andresartiga.system.bean.*;
-public class ProyectosService {
+import com.andresartiga.system.repository.ProyectosRepository;
+
+@Service
+public class ProyectosService implements IProyectosService {
+@Autowired
+
+private ProyectosRepository proyectosRepository;
+    @Override
+    public List<Proyectos> listarProyectos() {
+        return proyectosRepository.findAll();
+    }
+
+    @Override
+    public Proyectos buscarProyecto(Integer idProyecto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buscarProyecto'");
+    }
+
+    @Override
+    public Proyectos guardarProyecto(Proyectos proyectos) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'guardarProyecto'");
+    }
+
+    @Override
+    public void eliminarProyecto(Proyectos proyectos) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminarProyecto'");
+    }
+
 
 }
