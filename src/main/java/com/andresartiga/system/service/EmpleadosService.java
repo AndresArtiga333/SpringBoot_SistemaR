1a3070b1182748a699bb755b7f0c43516189943b
AndresArtiga333 <andresaartiga057@gmail.com>
2024-08-07 10:44:14 -0600

    feat: se agrego el metodo de listar empleados

--- src/main/java/com/andresartiga/system/service/EmpleadosService.java
+++ src/main/java/com/andresartiga/system/service/EmpleadosService.java
@@ -1,6 +1,8 @@
 package com.andresartiga.system.service;
 
 import com.andresartiga.system.bean.*;
+import com.andresartiga.system.repository.EmpleadosRepository;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,32 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmpleadosService implements IEmpleadosService{
+@Autowired
+
+private EmpleadosRepository empleadosRepository;
+    @Override
+    public List<Empleados> listarEmpleados() {
+      
+        return empleadosRepository.findAll();
+    }
+
+    @Override
+    public Empleados buscarEmpleado(Integer idEmpleado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buscarEmpleado'");
+    }
+
+    @Override
+    public Empleados guardarEmpleado(Empleados empleados) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'guardarEmpleado'");
+    }
+
+    @Override
+    public void eliminarEmpleado(Integer idEmpleados) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminarEmpleado'");
+    }
 
 
    
