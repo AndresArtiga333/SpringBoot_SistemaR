76fcb8478adf572d9c91050eb15a033f71543d23
AndresArtiga333 <andresaartiga057@gmail.com>
2024-08-07 07:44:28 -0600

    build: se creo el proyecto

---
+++ src/main/java/com/andresartiga/system/SpringBootSistemaRApplication.java
@@ -0,0 +1,13 @@
+package com.andresartiga.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSistemaRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSistemaRApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andresartiga/system/SpringBootSistemaRApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresartiga.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSistemaRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
