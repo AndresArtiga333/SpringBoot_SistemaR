77c7f198d3ea0d6190a99c63c9fed84da2fcd103
AndresArtiga333 <andresaartiga057@gmail.com>
2024-08-12 11:59:30 -0600

    feat: se agrego el listar y agregar de la entidad empleados, en el paquete de controller

--- src/main/java/com/andresartiga/system/controller/EmpleadoController.java
+++ src/main/java/com/andresartiga/system/controller/EmpleadoController.java
@@ -1,11 +1,42 @@
 package com.andresartiga.system.controller;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController //http://localhost:8081/rh-empleado
+import com.andresartiga.system.bean.Empleados;
+import com.andresartiga.system.service.IEmpleadosService;
+
+@RestController //http://localhost:8081/rh-empleados
 @RequestMapping ("rh-empleado")
 
 public class EmpleadoController {
 
+    private static final Logger logger = LoggerFactory.getLogger(EmpleadoController.class);
+
+    @Autowired /*Comunicacion entre el service y el repositorio */
+    private IEmpleadosService IEmpleadosService;
+        //http://localhost:8081/rh-empleados
+    @GetMapping("/empleados")
+
+    public List<Empleados> obtenerEmpleado(){
+        var empleado2 = IEmpleadosService.listarEmpleados();
+        empleado2.forEach(empleado -> logger.info(empleado.toString()));
+        return empleado2;
+    }
+
+    @PostMapping("/empleadosA")
+
+    public Empleados agregarEmpleado (@RequestBody Empleados empleados){
+        logger.info("Empleado agregado " + empleados);
+        return IEmpleadosService.guardarEmpleado(empleados);
+
+    }
 }
