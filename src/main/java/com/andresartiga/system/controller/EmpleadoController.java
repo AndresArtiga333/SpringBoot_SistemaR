90852c832bd8cdf6ac8fac9b389c0d7825048a35
AndresArtiga333 <andresaartiga057@gmail.com>
2024-08-12 10:55:54 -0600

    build: se agrego la carpeta de controller

---
+++ src/main/java/com/andresartiga/system/controller/EmpleadoController.java
@@ -0,0 +1,11 @@
+package com.andresartiga.system.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //http://localhost:8081/rh-empleado
+@RequestMapping ("rh-empleado")
+
+public class EmpleadoController {
+
+}
