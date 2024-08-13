ba625a20be8f41d8251cd8d6a9333c13704a5504
AndresArtiga333 <andresaartiga057@gmail.com>
2024-08-12 18:14:43 -0600

    feat: se agrego la clase de ProyectosController en el paquete controller

---
+++ src/main/java/com/andresartiga/system/controller/ProyectosController.java
@@ -0,0 +1,10 @@
+package com.andresartiga.system.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController  //http://localhost:8081/rh-proyectos
+@RequestMapping ("rh-proyectos")
+public class ProyectosController {
+    
+}
